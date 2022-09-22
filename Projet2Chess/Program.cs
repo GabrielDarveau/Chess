b33819fd880d74331bbdac7263f96756e4d52ad8
GabrielDarveau <2132614@etu.cchic.ca>
2022-09-22 12:06:33 -0400

    Vérification des coordonnées

--- Projet2Chess/Program.cs
+++ Projet2Chess/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Projet2Chess
 {
@@ -55,12 +56,55 @@ namespace Projet2Chess
 
         private static Coordonnee DemanderCoordonnees(string messageAAfficher)
         {
-            throw new NotImplementedException();
+            string rep;
+            Console.WriteLine(messageAAfficher);
+            rep = Console.ReadLine();
+            Coordonnee maCoord = DecortiquerCoordonnee(rep);
+            return maCoord;
+            //throw new NotImplementedException();
         }
 
         private static Coordonnee DecortiquerCoordonnee(string coordonneeADecortiquer)
         {
-            throw new NotImplementedException();
+            Dictionary<string, int> coord1 = new Dictionary<string, int>();
+            coord1.Add("A", 0);
+            coord1.Add("B", 1);
+            coord1.Add("C", 2);
+            coord1.Add("D", 3);
+            coord1.Add("E", 4);
+            coord1.Add("F", 5);
+            coord1.Add("G", 6);
+            coord1.Add("H", 7);
+
+            Dictionary<string, int> coord2 = new Dictionary<string, int>();
+            coord2.Add("1", 0);
+            coord2.Add("2", 1);
+            coord2.Add("3", 2);
+            coord2.Add("4", 3);
+            coord2.Add("5", 4);
+            coord2.Add("6", 5);
+            coord2.Add("7", 6);
+            coord2.Add("8", 7);
+
+            string caract1, caract2;
+            int nb1, nb2;
+            bool valide, verif;
+            Coordonnee maCoord;
+
+            caract1 = coordonneeADecortiquer.Substring(0, 1).ToUpper();
+            caract2 = coordonneeADecortiquer.Substring(1, 1);
+            verif = coord1.TryGetValue(caract1, out nb1);
+            valide = coord2.TryGetValue(caract2, out nb2);
+            if (valide && verif)
+            {
+                maCoord = new Coordonnee(nb1, nb2);
+                return maCoord;
+            }
+            else
+            {
+                return null;
+            }
+            //throw new NotImplementedException();
         }
 
         private static void InitialiserPartie()
