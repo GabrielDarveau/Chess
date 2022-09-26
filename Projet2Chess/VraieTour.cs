ec769a4934d99a417badf8168611e62235ac81c2
GabrielDarveau <2132614@etu.cchic.ca>
2022-09-26 11:39:43 -0400

    Pseudo code pour vérification des positions valides pour la tour

--- Projet2Chess/VraieTour.cs
+++ Projet2Chess/VraieTour.cs
@@ -13,9 +13,47 @@ namespace Projet2Chess
 
         public override List<Coordonnee> DeterminerPositionsValides(Piece[,] lePlateau, Coordonnee maPosition)
         {
+            /* valide : Booléen
+             * Si la couleur est blanche
+             *      Faire
+             *          Si la case du haut est vide et qu'elle est dans les limites du plateau
+             *              Ajouter cette case à la liste des positions valides
+             *              valide = vrai
+             *          Sinon
+             *              valide = faux
+             *      Tant que valide = vrai
+             *      
+             *      Faire
+             *          Si la case du bas est vide et qu'elle est dans les limites du plateau
+             *              Ajouter cette case à la liste des positions valides
+             *              valide = vrai
+             *          Sinon
+             *              valide = faux
+             *      Tant que valide = vrai
+             *      
+             *      Faire
+             *          Si la case de droite est vide et qu'elle est dans les limites du plateau
+             *              Ajouter cette case à la liste des positions valides
+             *              valide = vrai
+             *          Sinon
+             *              valide = faux
+             *      Tant que valide = vrai
+             *      
+             *      Faire
+             *          Si la case de gauche est vide et qu'elle est dans les limites du plateau
+             *              Ajouter cette case à la liste des positions valides
+             *              valide = vrai
+             *          Sinon
+             *              valide = faux
+             *      Tant que valide = vrai
+             *
+             *
+             */
+
             //throw new NotImplementedException();
             //Vous pouvez utiliser la ligne qui suit pour vérifier le comportement attendu
             return base.DeterminerPositionsValides(lePlateau, maPosition);
         }
+        
     }
 }
