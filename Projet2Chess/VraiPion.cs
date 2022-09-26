3b3d356dfb3a8aaba418c15e4e0363bd511563ed
NathanMorasse <2133133@etu.cchic.ca>
2022-09-26 12:41:59 -0400

    mouvements valides Pion

--- Projet2Chess/VraiPion.cs
+++ Projet2Chess/VraiPion.cs
@@ -13,27 +13,74 @@ namespace Projet2Chess
 
         public override List<Coordonnee> DeterminerPositionsValides(Piece[,] lePlateau, Coordonnee maPosition)
         {
-            /* int couleur = -1
-             * SI la couleur est blanche
-             *   couleur = 1
-             *      SI premier mouvement
-             *          SI la position en Y + couleur est vide et plus petite que les limites du tableau
+            /*   int mouvement = -1
+             *   int range = 7
+             *   SI la couleur est blanche
+             *   mouvement = 1
+             *   range 2
+             *   SI la rangée est plus petite que 8 et plus grande que 1
+             *      SI la position en Y + mouvement est vide
              *              ajouter cette position a la liste
-             *          SI les positions en Y + couleur et (Y + couleur) + couleur sont vide et plus petites que les limites du tableau
-             *              ajouter la deuxieme position à la liste
-             *      SINON
-             *          SI la position en Y + couleur est vide et plus petite que les limites du tableau
+             *              SI premier mouvement( position == range) 
+             *                  SI les positions en Y + mouvement et (Y + mouvement) + couleur sont vide
+             *                      ajouter la deuxieme position à la liste
+             *      SI la colone est plus grande que 1
+             *          SI la position en Y+mouvement X+1 est occupée par un ennemi
              *              ajouter cette position a la liste
-             *      SI la position en Y+couleur X+1 est occupée par un ennemi
-             *          ajouter cette position a la liste
-             *      SI la position en Y+ couleur X-1 est occupée par un ennemi
-             *          ajouter cette position a la liste
+             *      SI la colone est plus petite que 7
+             *          SI la position en Y+ mouvement X-1 est occupée par un ennemi
+             *               ajouter cette position a la liste
+             *          
             */
+            List<Coordonnee> coordonneesValides = new List<Coordonnee>();
+            int mouvement = -1, range = 7;
+            ConsoleColor couleur = ConsoleColor.Black;
+            if (this.couleurPiece == ConsoleColor.White)
+            {
+                couleur = ConsoleColor.White;
+                mouvement = 1;
+                range = 2;
+            }
 
+            if (maPosition.Y < 8 && maPosition.Y > 1)
+            {
+                if (VerifCaseVide(maPosition.X, maPosition.Y + mouvement))
+                {
+                    if (maPosition.Y == range) VerifCaseVide(maPosition.X, (maPosition.Y + mouvement) + mouvement);
+                }
 
+                if (maPosition.X < 1)
+                {
+                    VerifCaseEnnemie(maPosition.X + 1, maPosition.Y + mouvement);
+                }
+                if (maPosition.X > 7)
+                {
+                    VerifCaseEnnemie(maPosition.X - 1, maPosition.Y + mouvement);
+                }
+            }
+
+            bool VerifCaseVide(int x, int y)
+            {
+                if (typeof(PieceVide) == lePlateau[x, y].GetType())
+                {
+                    coordonneesValides.Add(new Coordonnee(x, y));
+                    return true;
+                }
+                return false;
+            }
+
+            void VerifCaseEnnemie(int x, int y)
+            {
+                if ((couleur == ConsoleColor.White && lePlateau[x,y].couleurPiece == ConsoleColor.Black) || (couleur == ConsoleColor.Black && lePlateau[x,y].couleurPiece == ConsoleColor.White))
+                {
+                    coordonneesValides.Add(new Coordonnee(x, y));
+                }
+            }
+
+            return coordonneesValides;
             //throw new NotImplementedException();
             //Vous pouvez utiliser la ligne qui suit pour vérifier le comportement attendu
-            return base.DeterminerPositionsValides(lePlateau, maPosition);
+            //return base.DeterminerPositionsValides(lePlateau, maPosition);
         }
 
     }
