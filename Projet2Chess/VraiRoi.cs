11a8562d4c014b8f755379591c03be4eb8dbdc37
NathanMorasse <morassenathan@gmail.com>
2022-09-29 12:18:44 -0400

    début roi

--- Projet2Chess/VraiRoi.cs
+++ Projet2Chess/VraiRoi.cs
@@ -13,9 +13,72 @@ namespace Projet2Chess
 
         public override List<Coordonnee> DeterminerPositionsValides(Piece[,] lePlateau, Coordonnee maPosition)
         {
+            /*  int mouvement = -1
+             *  SI la couleur est blanche
+             *      mouvement = 1
+             *  SI la position en haut N'est pas de la meme couleur
+             *      Ajouter cette position a la liste
+             *  SI la position au coin haut-droit N'est pas de la meme couleur
+             *      Ajouter cette position a la liste
+             *  SI la position à droite N'est pas de la meme couleur
+             *      Ajouter cette position a la liste
+             *  SI la position au coin bas-droite N'est pas de la meme couleur
+             *      Ajouter cette position a la liste
+             *  SI la position au bas N'est pas de la meme couleur
+             *      Ajouter cette position a la liste
+             *  SI la position au bas-gauche N'est pas de la meme couleur
+             *      Ajouter cette position a la liste
+             *  SI la position à gauche N'est pas de la meme couleur
+             *      Ajouter cette position a la liste
+             *  SI la position au coin haut-gauche N'est pas de la meme couleur
+             *      Ajouter cette position a la liste
+             */
+
+            List<Coordonnee> coordonneesValides = new List<Coordonnee>();
+            List<Coordonnee> cases = new List<Coordonnee>();
+            cases.Add(new Coordonnee(maPosition.X, maPosition.Y+1));
+            cases.Add(new Coordonnee(maPosition.X+1, maPosition.Y+1));
+            cases.Add(new Coordonnee(maPosition.X+1, maPosition.Y));
+            cases.Add(new Coordonnee(maPosition.X+1, maPosition.Y-1));
+            cases.Add(new Coordonnee(maPosition.X, maPosition.Y-1));
+            cases.Add(new Coordonnee(maPosition.X-1, maPosition.Y-1));
+            cases.Add(new Coordonnee(maPosition.X-1, maPosition.Y));
+            cases.Add(new Coordonnee(maPosition.X-1, maPosition.Y+1));
+
+
+            int mouvement = -1;
+            ConsoleColor couleur = ConsoleColor.Black;
+
+            if (this.couleurPiece == ConsoleColor.White)
+            {
+                couleur = ConsoleColor.White;
+                mouvement = 1;
+            }
+
+            foreach (Coordonnee caseAutour in cases)
+            {
+                VerifCase(caseAutour);
+            }
+
+            void VerifCase(Coordonnee caseAutour)
+            {
+                try
+                {
+                    if (lePlateau[caseAutour.X,caseAutour.Y].couleurPiece != couleur)
+                    {
+                        coordonneesValides.Add(caseAutour);
+                    }
+                }
+                catch (Exception e)
+                {
+                    
+                }
+            }
+            return coordonneesValides;
+
             //throw new NotImplementedException();
             //Vous pouvez utiliser la ligne qui suit pour vérifier le comportement attendu
-            return base.DeterminerPositionsValides(lePlateau, maPosition);
+            //return base.DeterminerPositionsValides(lePlateau, maPosition);
         }
     }
 }
