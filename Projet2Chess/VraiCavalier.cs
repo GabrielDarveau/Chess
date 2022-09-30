33d0f05cc6746f43d9955690e81911396a1ccdc9
NathanMorasse <2133133@etu.cchic.ca>
2022-09-30 14:06:22 -0400

    Cavalier fini

--- Projet2Chess/VraiCavalier.cs
+++ Projet2Chess/VraiCavalier.cs
@@ -16,11 +16,46 @@ namespace Projet2Chess
              *      SI la position n'est pas de la meme couleur
              *         Ajouter cette position a la liste
              */
+            List<Coordonnee> coordonneesValides = new List<Coordonnee>();
+            //Ajoute toutes les cases ou le cavalier peut se rendre dans une liste
+            List<Coordonnee> cases = new List<Coordonnee>();
+            cases.Add(new Coordonnee(maPosition.X-2, maPosition.Y + 1));
+            cases.Add(new Coordonnee(maPosition.X - 1, maPosition.Y + 2));
+            cases.Add(new Coordonnee(maPosition.X + 1, maPosition.Y + 2));
+            cases.Add(new Coordonnee(maPosition.X + 2, maPosition.Y +1));
+            cases.Add(new Coordonnee(maPosition.X + 2, maPosition.Y - 1));
+            cases.Add(new Coordonnee(maPosition.X + 1, maPosition.Y - 2));
+            cases.Add(new Coordonnee(maPosition.X - 1, maPosition.Y - 2));
+            cases.Add(new Coordonnee(maPosition.X - 2, maPosition.Y -1));
 
+            ConsoleColor couleur = this.couleurPiece;
+
+            //Vérifie chaque case
+            foreach (Coordonnee caseAutour in cases)
+            {
+                VerifCase(caseAutour);
+            }
+
+            //vérifie si une case est occuper par un allié, si non, la case est valide et est ajoutée à la liste
+            void VerifCase(Coordonnee caseAutour)
+            {
+                try
+                {
+                    if (lePlateau[caseAutour.X, caseAutour.Y].couleurPiece != couleur)
+                    {
+                        coordonneesValides.Add(caseAutour);
+                    }
+                }
+                catch (IndexOutOfRangeException e)
+                {
+
+                }
+            }
+            return coordonneesValides;
 
             //throw new NotImplementedException();
             //Vous pouvez utiliser la ligne qui suit pour vérifier le comportement attendu
-            return base.DeterminerPositionsValides(lePlateau, maPosition);
+            //return base.DeterminerPositionsValides(lePlateau, maPosition);
         }
     }
 }
